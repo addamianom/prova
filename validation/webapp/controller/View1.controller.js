817213a6777a64a653c06c7e68633d780ec71b2a
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-03 21:08:32 +0200

    commit1

--- validation/webapp/controller/View1.controller.js
+++ validation/webapp/controller/View1.controller.js
@@ -5,6 +5,14 @@ sap.ui.define([
 
     return Controller.extend("com.demo.validation.validation.controller.View1", {
         onInit() {
+        },
+
+         onSubmit() {
+            var empid = this.getView().byId("empid").getValue();
+            if (empid === ""){
+             this.getView().byId("empid").setValueState("Error");
+              this.getView().byId("empid").setValueStateText("Employeid is mandatory please fill the field");
+            }     
         }
     });
 });
