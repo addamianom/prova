f7c395ea86ef9347d550ed2ebb69c2efae5afaf3
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-03 12:43:10 +0200

    first commit grid layout

---
+++ gridlayout/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.demo.gridlayout.gridlayout.controller.View1", {
+        onInit() {
+        }
+    });
+});
