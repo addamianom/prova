827db90fb7c2e2cdec7cb330326f6eb75d8814c9
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-04 15:39:33 +0200

    validation commit fine

--- validation/webapp/controller/View1.controller.js
+++ validation/webapp/controller/View1.controller.js
@@ -7,13 +7,35 @@ sap.ui.define([
         onInit() {
         },
 
-         onSubmit() {
+        onSubmit() {
             var empid = this.getView().byId("empid").getValue();
-            if (empid === ""){
-             this.getView().byId("empid").setValueState("Error");
-              this.getView().byId("empid").setValueStateText("Employeid is mandatory please fill the field");
-            } else {   
-               this.getView().byId("empid").setValueState("None"); 
+            if (empid === "") {
+                this.getView().byId("empid").setValueState("Error");
+                this.getView().byId("empid").setValueStateText("Employeid is mandatory please fill the field");
+            } else {
+                this.getView().byId("empid").setValueState("None");
+
+
+                // var regex = /^[a-zA-Z ]+$/; Alphabet validation
+                // var regex = /^[0-9]+$/; Numeric validation
+                // employeeid must be alphanumeric validation
+                var regex = /^[a-zA-Z0-9 ]+$/;
+                if (!empid.match(regex)) {
+                    this.getView().byId("empid").setValueState("Error");
+                    this.getView().byId("empid").setValueStateText("Employeid must contain only letters and numbers");
+                } else {
+                    this.getView().byId("empid").setValueState("None");
+
+                    // format validation
+                    if (empid.length < 10) {
+                        this.getView().byId("empid").setValueState("Error");
+                        this.getView().byId("empid").setValueStateText("Employeid must have 10 digits");
+                    } else {
+                        this.getView().byId("empid").setValueState("None");
+
+                    }
+                }
+            }
         }
     });
 });
