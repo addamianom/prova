3c956996b5fcfbc108a367fe3af5b7cb85097e99
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-06 18:49:25 +0200

    xommit json model

---
+++ zb70json/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("com.demo.zb70json.controller.App", {
+      onInit() {
+      }
+  });
+});
