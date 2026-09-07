7e65a5af3aa719c9e3cb6d08a23c5ef22822c2f5
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-07 19:25:35 +0200

    commit2

--- zb70jsonfield/webapp/Component.js
+++ zb70jsonfield/webapp/Component.js
@@ -1,9 +1,12 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
 
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/Device",
-    "com/demo/zb70jsonfield/model/models"
-],
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "com/demo/zb70json1/model/models"
+    ],
     function (UIComponent, Device, models) {
         "use strict";
 
@@ -12,8 +15,12 @@ sap.ui.define([
                 manifest: "json"
             },
 
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
             init: function () {
-
                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
 
@@ -22,22 +29,19 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
-
-
-
                 // json model
                 var oModel = this.getModel(); // gives odata model
                 var empModel = this.getModel("empModel"); // gives json model 
-                oModel.read("/EmployeeSet"), {
-                    success: function (data) {
+                oModel.read("/EmployeeSet", {
+                    success: function(data) {
                         empModel.setData(data);
-                    },
-                    error: function (error) {
-                        console.log(error);
+                        },
+                    error:function(error) {
+                       // console.log(error);
                     }
-                }
+                });
             }
-
+    
         });
     }
 );
--- zb70jsonfield/webapp/controller/View1.controller.js
+++ zb70jsonfield/webapp/controller/View1.controller.js
@@ -5,7 +5,6 @@ sap.ui.define([
     function (UIComponent) {
         "use strict";
 
-
     return Controller.extend("com.demo.zb70jsonfield.controller.View1", {
         onInit: function () {
 
