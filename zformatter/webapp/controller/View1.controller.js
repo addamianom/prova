e0e9b971f2879a6d8aa9d20999d4b4a729ffb9fd
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-08 12:06:40 +0200

    commit2

--- zformatter/webapp/controller/View1.controller.js
+++ zformatter/webapp/controller/View1.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "com/demo/zformatter/model/formatter"
 ],
-function (Controller) {
+function (Controller, formatter) {
     "use strict";
 
     return Controller.extend("com.demo.zformatter.controller.View1", {
+        f: formatter,
         onInit: function () {
 
         }
--- zformatter/webapp/model/formatter.js
+++ zformatter/webapp/model/formatter.js
@@ -1,7 +1,18 @@
-sap.ui.define( function () 
-    {
-        "use strict";
+sap.ui.define(function () {
+    "use strict";
     return {
+        formatName: function (Name) {
+            return "Mr. " + Name;
+        },
+        colorSkill: function (Skill) {
+            if (Skill === "ABAP") {
+                return "Success";
+            } else if (Skill === "JAVA") {
+                return "Warning";
+            } else {
+                return "Error";
+            }
+        }
 
-           };
+      };
     });
