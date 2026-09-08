24fa9f6e8c7f14850090dd31c4a10eba99b16cde
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-08 18:30:10 +0200

    commit3

--- zb70fragment/webapp/controller/View1.controller.js
+++ zb70fragment/webapp/controller/View1.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "com/demo/zb70fragment/model/formatter"
+], (Controller, formatter) => {
     "use strict";
 
     return Controller.extend("com.demo.zb70fragment.controller.View1", {
+        f: formatter,
         onInit() {
         },
         onPress: function () {
