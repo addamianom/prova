b3512557a63252f5eb1346203c3bb491d51d5fe2
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-09 22:12:52 +0200

    commit1

--- zb70passa/webapp/controller/View1.controller.js
+++ zb70passa/webapp/controller/View1.controller.js
@@ -1,12 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "com/demo/zb70passa/model/formatter"
+
 ],
-function (Controller) {
+function (Controller, formatter) {
     "use strict";
 
     return Controller.extend("com.demo.zb70passa.controller.View1", {
+        f: formatter,
         onInit: function () {
 
+        },
+        onSubmit: function () {
+            this.getOwnerComponent().getRouter().navTo("RouteView2");
+           // let oRouter = this().getOwnerComponent().getRouter();
+            // oRouter.navTo("RouteView2");
         }
     });
 });
---
+++ zb70passa/webapp/controller/View2.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+     "com/demo/zb70passa/model/formatter"
+],
+function (Controller, formatter) {
+    "use strict";
+
+    return Controller.extend("com.demo.zb70passa.controller.View2", {
+        onInit: function () {
+
+        }
+    });
+});
