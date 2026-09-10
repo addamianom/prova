d9280bc15b755396aa7e0ed5a29655b31bfb9fdc
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-10 12:12:47 +0200

    commit5

--- zb70passa/webapp/controller/View2.controller.js
+++ zb70passa/webapp/controller/View2.controller.js
@@ -1,31 +1,28 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    
-     "com/demo/zb70passa/model/formatter",
       "sap/ui/model/json/JSONModel"
 ],
-function (Controller, formatter, JSONModel) {
+function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("com.demo.zb70passa.controller.View2", {
-        fr: formatter,
+
         onInit: function () {
 
- this.getOwnerComponent().getRouter.getRoute("RouteView2").attachPatternMatched(this.PatternMatched, this)
-
-        },
+ this.getOwnerComponent().getRouter().getRoute("RouteView2").attachPatternMatched(this.PatternMatched, this);
+ 
+},
         onPatternMatched: function () {
-            this.getOwnerComponent().getModel().getProperty("/id");
+             this.getOwnerComponent().getModel().getProperty("/id");
             this.getOwnerComponent().getModel().getProperty("/order");
 
             this.getView().byId("inpid1").setText("id");
             this.getView().byId("inpOrderno1").setText("order");
-
+ 
         },
         onBack: function (){
              this.getOwnerComponent().getRouter().navTo("RouteView1");
               
-
         }
 
         });
