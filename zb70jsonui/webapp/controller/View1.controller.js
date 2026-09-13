1257b3e1791360257a616785380b7e1ae01b8d46
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-13 22:44:16 +0200

    commit6

--- zb70jsonui/webapp/controller/View1.controller.js
+++ zb70jsonui/webapp/controller/View1.controller.js
@@ -4,50 +4,83 @@ sap.ui.define([
     "com/demo/zb70jsonui/model/formatter",
     "sap/ui/model/json/JSONModel"
 ],
-function (Controller, formatter, JSONModel) {
-    "use strict";
+    function (Controller, formatter, JSONModel) {
+        "use strict";
 
-    return Controller.extend("com.demo.zb70jsonui.controller.View1", {
-        fr: formatter,
-        onInit: function () {
+        return Controller.extend("com.demo.zb70jsonui.controller.View1", {
+            fr: formatter,
+            onInit: function () {
 
-        },
-        onPress: function () {
-          
-            this.getOwnerComponent().getRouter().navTo("RouteView2");
-           // let oRouter = this().getOwnerComponent().getRouter();
-            // oRouter.navTo("RouteView2");
-         },
-        onSelRealtion: function (oEvent) {
-            var serrel = oEvent.getParameter("selectedItem").getText();
-            console.log(serrel);
-        },
-        onSelRelCB: function (oEvent) {
-             var serrel = oEvent.getParameter("selectedItem").getText();
-            console.log(serrel);
-        } ,
-        onSelFin: function (oEvent) {
-            
-           var aRelitems = oEvent.getParameter("selectedItems");
-           for (var i=0;i<aRelitems.length;i++) {
-            
-            console.log(aRelitems[i].getText());
-           }
-           },
+            },
+            onPress: function () {
 
-           onSelrelb: function (oEvent) {
-             var index = oEvent.getParameter("selectedIndex");
-             if (index=== 0 ) {
-                console.log("Italian");
-             } else {         console.log("English"); }
+                this.getOwnerComponent().getRouter().navTo("RouteView2");
+                // let oRouter = this().getOwnerComponent().getRouter();
+                // oRouter.navTo("RouteView2");
+            },
+            onSelRealtion: function (oEvent) {
+                var serrel = oEvent.getParameter("selectedItem").getText();
+                console.log(serrel);
+
+                var oModel = this.getView().getModel("testata");
+                oModel.setProperty("/select", serrel);
+            },
+            onSelRelCB: function (oEvent) {
+                var serrel = oEvent.getParameter("selectedItem").getText();
+                console.log(serrel);
+
+                var oModel = this.getView().getModel("testata");
+                oModel.setProperty("/combobox", serrel);
+            },
+            onSelFin: function (oEvent) {
+                var serrel;
+                var aRelitems = oEvent.getParameter("selectedItems");
+                for (var i = 0; i < aRelitems.length; i++) {
                   
-        
-        
-        
-        }
-           
+                    var sText = aRelitems[i].getText();
+                    console.log(sText);
 
+                    // 2. Unisci i testi. Qui ho aggiunto una virgola e uno spazio ", " per separarli
+                    if (serrel === "") {
+                        serrel = sText;
+                    } else {
+                        serrel = serrel + ", " + sText;
+                    }
+                    var oModel = this.getView().getModel("testata");
+                    if (oModel) {
 
-        
+                        oModel.setProperty("/multicombobox", serrel);
+                    }
+                }
+            },
+
+            onSelrelb: function (oEvent) {
+                var oModel = this.getView().getModel("testata");
+                var index = oEvent.getParameter("selectedIndex");
+                if (index === 0) {
+                    console.log("Italian");
+                    oModel.setProperty("/radio", "Italian");
+                } else {
+                    if (index === 1) {
+                        console.log("English");
+                        oModel.setProperty("/radio", "English");
+                    } else {
+                        if (index === 2) {
+                            console.log("French");
+                            oModel.setProperty("/radio", "French");
+                        }
+                    }
+                }
+
+            },
+            onSelChb: function (oEvent) {
+                var bchecked = oEvent.getParameter("selected");
+                if (bchecked === true) {
+                    console.log("Selected")
+                } else {
+                    console.log("DeSelected")
+                }
+            }
+
+        });
     });
-});
