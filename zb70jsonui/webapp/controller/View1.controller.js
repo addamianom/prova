1d40ac455030a07a029f7e1a6c269a0ae5537322
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-13 18:54:12 +0200

    commit3

--- zb70jsonui/webapp/controller/View1.controller.js
+++ zb70jsonui/webapp/controller/View1.controller.js
@@ -17,6 +17,37 @@ function (Controller, formatter, JSONModel) {
             this.getOwnerComponent().getRouter().navTo("RouteView2");
            // let oRouter = this().getOwnerComponent().getRouter();
             // oRouter.navTo("RouteView2");
+         },
+        onSelRealtion: function (oEvent) {
+            var serrel = oEvent.getParameter("selectedItem").getText();
+            console.log(serrel);
+        },
+        onSelRelCB: function (oEvent) {
+             var serrel = oEvent.getParameter("selectedItem").getText();
+            console.log(serrel);
+        } ,
+        onSelFin: function (oEvent) {
+            
+           var aRelitems = oEvent.getParameter("selectedItems");
+           for (var i=0;i<aRelitems.length;i++) {
+            
+            console.log(aRelitems[i].getText());
+           }
+           },
+
+           onSelrelb: function (oEvent) {
+             var index = oEvent.getParameter("selectedIndex");
+             if (index=== 0 ) {
+                console.log("Italian");
+             } else {         console.log("English"); }
+                  
+        
+        
+        
         }
+           
+
+
+        
     });
 });
