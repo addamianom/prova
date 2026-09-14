aa9e9236cd0fa0ea564525f5dbbb92aa63b1d2a7
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-14 09:13:20 +0200

    commit1

--- zb70odatalist/webapp/Component.js
+++ zb70odatalist/webapp/Component.js
@@ -5,9 +5,11 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "com/demo/zb70odatalist/model/models"
+        "com/demo/zb70odatalist/model/models",
+         "sap/ui/model/json/JSONModel"
+
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, JSONModel) {
         "use strict";
 
         return UIComponent.extend("com.demo.zb70odatalist.Component", {
@@ -20,16 +22,26 @@ sap.ui.define([
              * @public
              * @override
              */
-            init: function () {
-                // call the base component's init function
+              init: function () {
+
+                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
 
+               // var oModel = new JSONModel("model/data.json");
+ 
+             const oModel = new sap.ui.model.json.JSONModel();
+            const sPath = sap.ui.require.toUrl("com/demo/zb70jsonui/model/data.json");
+            oModel.loadData(sPath); 
+  
+                this.setModel(oModel, "testata");
+            
                 // enable routing
                 this.getRouter().initialize();
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
             }
+
         });
     }
 );
