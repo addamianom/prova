ec54c4d0a22f9ba39691e81c57d0ab0d8901f442
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-14 16:36:40 +0200

    commit 6

--- zb70odatalist/webapp/controller/View1.controller.js
+++ zb70odatalist/webapp/controller/View1.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/demo/zb70odatalist/model/formatter",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+     "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter"
+
 ],
-    function (Controller, formatter, JSONModel) {
+    function (Controller, formatter, JSONModel,Filter, FilterOperator, Sorter ) {
         "use strict";
 
         return Controller.extend("com.demo.zb70odatalist.controller.View1", {
@@ -18,17 +22,52 @@ sap.ui.define([
                 // oRouter.navTo("RouteView2");
             },
             onSelEmp: function (oEvent) {
-            /* var employe =  oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
-                console.log(employe); */
-              var  empid =  oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
+                /* var employe =  oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
+                    console.log(employe); */
+                var empid = oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
 
-            // nel json model
-               // var sPath =  oEvent.getParameter("listItem").getBindingContext().getPath();
-               // var index = sPath.split("/")[1];
-               //  alert(index);
-               this.getOwnerComponent().getRouter().navTo("RouteView2", { Index: empid });
+                // nel json model
+                // var sPath =  oEvent.getParameter("listItem").getBindingContext().getPath();
+                // var index = sPath.split("/")[1];
+                //  alert(index);
+                this.getOwnerComponent().getRouter().navTo("RouteView2", { Index: empid });
+            },
+
+
+            onSearch: function (oEvent) {
+                // Handle search event
+                var value = oEvent.getParameter("newValue");
+                var afilter = [];
+                if (value !== "") {
+                    var oFilter = new Filter("Name", FilterOperator.Contains, value);
+
+                    // Crea il filtro per la colonna Skill
+                    var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
+
+                    // Unisce i due filtri in un unico filtro logico OR (and = false)
+                    var oCombinedFilter = new sap.ui.model.Filter({
+                        filters: [oFilter, oFilterSkill],
+                        and: false
+                    });
+
+
+                    afilter.push(oCombinedFilter);
+
+                }
+                this.getView().byId("list").getBinding("items").filter(afilter);
+
+            },
+            sortAsc: function () {
+                var oSorter = new Sorter("Name", false);
+                this.getView().byId("list").getBinding("items").sort(oSorter);
+
+            },
+
+            sortDesc: function () {
+                var oSorter = new Sorter("Name", true);
+                this.getView().byId("list").getBinding("items").sort(oSorter);
             }
-            
+
 
         });
     });
