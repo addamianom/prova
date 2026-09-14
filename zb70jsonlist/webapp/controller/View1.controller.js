05d2a98e7a68965616b2ecd31c9a57c82fb02b61
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-14 17:34:56 +0200

    commit2

--- zb70jsonlist/webapp/controller/View1.controller.js
+++ zb70jsonlist/webapp/controller/View1.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
     "sap/ui/model/Sorter"
 
 ],
-    function (Controller, formatter, JSONModel,  Filter, FilterOperator, Sorte) {
+    function (Controller, formatter, JSONModel, Filter, FilterOperator, Sorte) {
         "use strict";
 
         return Controller.extend("com.demo.zb70jsonlist.controller.View1", {
@@ -22,53 +22,55 @@ sap.ui.define([
                 // oRouter.navTo("RouteView2");
             },
             onSelEmp: function (oEvent) {
-            /* var employe =  oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
-                console.log(employe); */
-              var  empid =  oEvent.getParameter("listItem").getBindingContext().getProperty("Empid");
-                  alert(index);
-            // nel json model
-               // var sPath =  oEvent.getParameter("listItem").getBindingContext().getPath();
-               // var index = sPath.split("/")[1];
-               //  alert(index);
-               this.getOwnerComponent().getRouter().navTo("RouteView2", { Index: empid });
+                // 1. Recuperi l'elemento selezionato (funziona sia per List che per Table)
+                var oSelectedItem = oEvent.getParameter("listItem") || oEvent.getSource();
+
+                // 2. Recuperi il valore della proprietà "Empid" dal modello
+                var sEmpId = oSelectedItem.getBindingContext().getProperty("Empid");
+
+                // 3. Navighi alla seconda view passando l'Empid
+                this.getOwnerComponent().getRouter().navTo("RouteView2", {
+                    Index: sEmpId
+                });
+              //  this.getOwnerComponent().getRouter().navTo("RouteView2", { Index: index });
             },
 
-             onSearch: function (oEvent) {
-            // Handle search event
-            var value = oEvent.getParameter("newValue");
-            var afilter = [];
-            if (value !== "") {
-                var oFilter = new Filter("Name", FilterOperator.Contains, value);
+            onSearch: function (oEvent) {
+                // Handle search event
+                var value = oEvent.getParameter("newValue");
+                var afilter = [];
+                if (value !== "") {
+                    var oFilter = new Filter("Name", FilterOperator.Contains, value);
 
-                // Crea il filtro per la colonna Skill
-                var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
+                    // Crea il filtro per la colonna Skill
+                    var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
 
-                // Unisce i due filtri in un unico filtro logico OR (and = false)
-                var oCombinedFilter = new sap.ui.model.Filter({
-                    filters: [oFilter, oFilterSkill],
-                    and: false
-                });
+                    // Unisce i due filtri in un unico filtro logico OR (and = false)
+                    var oCombinedFilter = new sap.ui.model.Filter({
+                        filters: [oFilter, oFilterSkill],
+                        and: false
+                    });
 
 
-                afilter.push(oCombinedFilter);
+                    afilter.push(oCombinedFilter);
 
+                }
+                this.getView().byId("idlist").getBinding("items").filter(afilter);
+
+            },
+            sortAsc: function () {
+                var oSorter = new Sorter("Name", false);
+                this.getView().byId("idlist").getBinding("items").sort(oSorter);
+
+            },
+
+            sortDesc: function () {
+                var oSorter = new Sorter("Name", true);
+                this.getView().byId("idlist").getBinding("items").sort(oSorter);
             }
-            this.getView().byId("idlist").getBinding("items").filter(afilter);
-
-        },
-        sortAsc: function () {
-            var oSorter = new Sorter("Name", false);
-            this.getView().byId("idlist").getBinding("items").sort(oSorter);
-
-        },
-
-        sortDesc: function () {
-            var oSorter = new Sorter("Name", true);
-            this.getView().byId("idlist").getBinding("items").sort(oSorter);
-        }
 
 
-            
+
 
         });
     });
