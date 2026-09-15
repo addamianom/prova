601f009f8989fab6171d8b6f51a157eafe015730
Michele Addamiano <addamianom@gmail.com>
2026-09-15 08:00:27 +0200

    SAPUI5 odata model gui table search filter (#6)
    * commit1
    
    * commit2
    
    * commit2
    
    * commit4
    
    * commit 5
    
    * first commit zb70
    
    * commit2
    
    * commit3
    
    * First commit zb70jsonui
    
    * commit1
    
    * commit1
    
    * commit 2
    
    * commit2
    
    * commit3
    
    * commit6
    
    * commit 7
    
    * first commit zb70odatalist
    
    * commit1
    
    * commit2
    
    * commit4
    
    * commit5
    
    * commit 6
    
    * firtst commit zbt0jsonlist
    
    * commit1
    
    * commit2
    
    * commit4
    
    * commit7
    
    * commit 8
    
    * first commit zb70odatatable
    
    * commit1
    
    * commit3
    
    ---------
    
    Co-authored-by: Michele Addamiano <michele.addamiano2@neorizon.it>

---
+++ zb70odatatable/webapp/controller/View2.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+ "com/demo/zb70odatatable/model/formatter",
+    "sap/ui/model/json/JSONModel"
+],
+    function (Controller, formatter, JSONModel) {
+        "use strict";
+
+        return Controller.extend("com.demo.zb70odatatable.controller.View2", {
+           fr:formatter,
+            onInit: function () {
+                this.getOwnerComponent().getRouter().
+                    getRoute("RouteView2").attachPatternMatched(this.onPatternMatched, this);
+            },
+            onPatternMatched: function (oEvent) {
+               var index = oEvent.getParameter("arguments").Index;
+               this.getView().bindElement("/EmployeeSet('" + index + "')" );
+
+            },
+            onBack: function () {
+                this.getOwnerComponent().getRouter().navTo("RouteView1");
+            }
+
+        });
+    });
+
+
+
