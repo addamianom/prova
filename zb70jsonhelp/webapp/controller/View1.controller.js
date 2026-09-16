6cb614e95b0b20b5bdd10175e3f0e411f30bb5d6
Michele Addamiano <michele.addamiano2@neorizon.it>
2026-09-17 00:15:24 +0200

    commit2

--- zb70jsonhelp/webapp/controller/View1.controller.js
+++ zb70jsonhelp/webapp/controller/View1.controller.js
@@ -66,7 +66,63 @@ sap.ui.define([
             sortDesc: function () {
                 var oSorter = new Sorter("Name", true);
                 this.getView().byId("table").getBinding("items", "empModel").sort(oSorter);
+            },
+            onValueHelpPress: function () {
+                if (!this._dialog){
+
+              this._dialog = sap.ui.xmlfragment(this.getView().getId(), 
+                  "com.demo.zb70jsonhelp.view.EmpF4Help", this);
+                  this.getView().addDependent(this._dialog);
             }
+                this._dialog.open();
+             },
+
+         onSearchhelp: function (oEvent) {
+                // Handle search event
+                var value = oEvent.getParameter("newValue");
+                var afilter = [];
+                if (value !== "") {
+                    var oFilter = new Filter("Name", FilterOperator.Contains, value);
+
+                    // Crea il filtro per la colonna Skill
+                    var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
+
+                    // Unisce i due filtri in un unico filtro logico OR (and = false)
+                    var oCombinedFilter = new sap.ui.model.Filter({
+                        filters: [oFilter, oFilterSkill],
+                        and: false
+                    });
+
+
+                    afilter.push(oCombinedFilter);
+
+                }
+                this.getView().byId("table1").getBinding("items","empModel").filter(afilter);
+
+            },
+            sortAschelp: function () {
+                var oSorter = new Sorter("Name", false);
+                this.getView().byId("table1").getBinding("items", "empModel").sort(oSorter);
+
+            },
+            sortDeschelp: function () {
+                var oSorter = new Sorter("Name", true);
+                this.getView().byId("table1").getBinding("items", "empModel").sort(oSorter);
+            },
+
+           onClose: function () {
+              this._dialog.close();
+           }
+        /*    onSelEmpfromHelp: function (oEvent) {
+            
+                // 2. Recuperi il valore della proprietà "Empid" dal modello
+var sEmpId = oEvent.getParameter("listItem").getBindingContext("empModel").getProperty("Empid");
+               
+              this.getView().byId("inpOrderno").setValue(sEmpId);
+              if (this._dialog) {
+                 this._dialog.close();
+                } 
+           } */
 
         });
     });
