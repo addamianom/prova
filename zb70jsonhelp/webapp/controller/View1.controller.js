bd02b21257b26079cee37169be7e3c8b8b5e8d1a
Michele Addamiano <addamianom@gmail.com>
2026-09-17 17:32:32 +0200

    SAPUI5 JSON F4 HELP (#8)
    * commit1
    
    * commit2
    
    * commit2
    
    * commit4
    
    * commit 5
    
    * first commit zb70
    
    * commit2
    
    * commit3
    
    * First commit zb70jsonui
    
    * commit1
    
    * commit1
    
    * commit 2
    
    * commit2
    
    * commit3
    
    * commit6
    
    * commit 7
    
    * first commit zb70odatalist
    
    * commit1
    
    * commit2
    
    * commit4
    
    * commit5
    
    * commit 6
    
    * firtst commit zbt0jsonlist
    
    * commit1
    
    * commit2
    
    * commit4
    
    * commit7
    
    * commit 8
    
    * first commit zb70odatatable
    
    * commit1
    
    * commit3
    
    * first commit zb70jsontable
    
    * commit2
    
    * commit1
    
    * first commit zb70jsonhelp
    
    * commit1
    
    * commit2
    
    * commit3
    
    * commit4
    
    ---------
    
    Co-authored-by: Michele Addamiano <michele.addamiano2@neorizon.it>

---
+++ zb70jsonhelp/webapp/controller/View1.controller.js
@@ -0,0 +1,129 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "com/demo/zb70jsonhelp/model/formatter",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter"
+],
+    function (Controller, formatter, JSONModel, Filter, FilterOperator, Sorter) {
+        "use strict";
+
+        return Controller.extend("com.demo.zb70jsonhelp.controller.View1", {
+            f: formatter,
+            onInit: function () {
+
+            },
+            onPress: function () {
+
+                this.getOwnerComponent().getRouter().navTo("RouteView2");
+                // let oRouter = this().getOwnerComponent().getRouter();
+                // oRouter.navTo("RouteView2");
+            },
+            onSelEmp: function (oEvent) {
+                // 1. Recuperi l'elemento selezionato (funziona sia per List che per Table)
+                var oSelectedItem = oEvent.getParameter("listItem") || oEvent.getSource();
+
+                // 2. Recuperi il valore della proprietà "Empid" dal modello
+                var sEmpId = oSelectedItem.getBindingContext("empModel").getProperty("Empid");
+
+                // 3. Navighi alla seconda view passando l'Empid
+                this.getOwnerComponent().getRouter().navTo("RouteView2", {
+                    Index: sEmpId
+                });
+              //  this.getOwnerComponent().getRouter().navTo("RouteView2", { Index: index });
+            },
+
+            onSearch: function (oEvent) {
+                // Handle search event
+                var value = oEvent.getParameter("newValue");
+                var afilter = [];
+                if (value !== "") {
+                    var oFilter = new Filter("Name", FilterOperator.Contains, value);
+
+                    // Crea il filtro per la colonna Skill
+                    var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
+
+                    // Unisce i due filtri in un unico filtro logico OR (and = false)
+                    var oCombinedFilter = new sap.ui.model.Filter({
+                        filters: [oFilter, oFilterSkill],
+                        and: false
+                    });
+
+
+                    afilter.push(oCombinedFilter);
+
+                }
+                this.getView().byId("table").getBinding("items","empModel").filter(afilter);
+
+            },
+            sortAsc: function () {
+                var oSorter = new Sorter("Name", false);
+                this.getView().byId("table").getBinding("items", "empModel").sort(oSorter);
+
+            },
+            sortDesc: function () {
+                var oSorter = new Sorter("Name", true);
+                this.getView().byId("table").getBinding("items", "empModel").sort(oSorter);
+            },
+            onValueHelpPress: function () {
+                if (!this._dialog){
+
+              this._dialog = sap.ui.xmlfragment(this.getView().getId(), 
+                  "com.demo.zb70jsonhelp.view.EmpF4Help", this);
+                  this.getView().addDependent(this._dialog);
+            }
+                this._dialog.open();
+             },
+
+         onSearchhelp: function (oEvent) {
+                // Handle search event
+                var value = oEvent.getParameter("newValue");
+                var afilter = [];
+                if (value !== "") {
+                    var oFilter = new Filter("Name", FilterOperator.Contains, value);
+
+                    // Crea il filtro per la colonna Skill
+                    var oFilterSkill = new sap.ui.model.Filter("Skill", FilterOperator.Contains, value);
+
+                    // Unisce i due filtri in un unico filtro logico OR (and = false)
+                    var oCombinedFilter = new sap.ui.model.Filter({
+                        filters: [oFilter, oFilterSkill],
+                        and: false
+                    });
+
+
+                    afilter.push(oCombinedFilter);
+
+                }
+                this.getView().byId("table1").getBinding("items","empModel").filter(afilter);
+
+            },
+            sortAschelp: function () {
+                var oSorter = new Sorter("Name", false);
+                this.getView().byId("table1").getBinding("items", "empModel").sort(oSorter);
+
+            },
+            sortDeschelp: function () {
+                var oSorter = new Sorter("Name", true);
+                this.getView().byId("table1").getBinding("items", "empModel").sort(oSorter);
+            },
+
+           onClose: function () {
+              this._dialog.close();
+           },
+           onSelEmpHelp: function (oEvent) {
+            
+                // 2. Recuperi il valore della proprietà "Empid" dal modello
+           var sEmpId = oEvent.getParameter("listItem").getBindingContext("empModel").getProperty("Empid");
+               
+              this.getView().byId("inpOrderno").setValue(sEmpId);
+              if (this._dialog) {
+                 this._dialog.close();
+                } 
+           } 
+
+        });
+    });
+
+
---
+++ zb70jsonhelp/webapp/controller/View2.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+ "com/demo/zb70jsonhelp/model/formatter",
+    "sap/ui/model/json/JSONModel"
+],
+    function (Controller, formatter, JSONModel) {
+        "use strict";
+
+        return Controller.extend("com.demo.zb70jsonhelp.controller.View2", {
+           fr:formatter,
+            onInit: function () {
+                this.getOwnerComponent().getRouter().
+                    getRoute("RouteView2").attachPatternMatched(this.onPatternMatched, this);
+            },
+            onPatternMatched: function (oEvent) {
+               var index = oEvent.getParameter("arguments").Index;
+               this.getView().bindElement("/EmployeeSet('" + index + "')" );
+
+            },
+            onBack: function () {
+                this.getOwnerComponent().getRouter().navTo("RouteView1");
+            }
+
+        });
+    });
